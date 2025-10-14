0029a2017ca87d3e0149250c30d3aa057798105e
estevez10 <juanpabloestevez98@gmail.com>
2025-10-14 18:27:39 -0500

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,11 @@ import { AdminComponent } from './theme/layout/admin/admin.component';
 import { UsuarioComponent } from './demo/pages/usuario/usuario.component';
 import { MedicoComponent } from './demo/pages/medico/medico.component';
 import { PacienteComponent } from './demo/pages/paciente/paciente.component';
+import { CitasComponent } from './demo/pages/citas/citas.component';
+import { EspecializacionComponent } from './demo/pages/especializacion/especializacion.component';
+import { FormulasMedicasComponent } from './demo/pages/formulas-medicas/formulas-medicas.component';
+import { HistoriasMedicasComponent } from './demo/pages/historias-medicas/historias-medicas.component';
+import { MedicamentosComponent } from './demo/pages/medicamentos/medicamentos.component';
 
 export const routes: Routes = [
   {
@@ -18,7 +23,12 @@ export const routes: Routes = [
     children: [      
        { path: 'usuario', component: UsuarioComponent, data: { title: 'Usuario' }},
        { path: 'medico', component: MedicoComponent, data: { title: 'Medico' }},
-       { path: 'paciente', component: PacienteComponent, data: { title: 'Paciente' }} 
+       { path: 'paciente', component: PacienteComponent, data: { title: 'Paciente' }},
+       { path: 'citas', component: CitasComponent, data: { title: 'Citas' }},
+       { path: 'especializacion', component: EspecializacionComponent, data: { title: 'Especializacion' }},
+       { path: 'formulas-medicas', component: FormulasMedicasComponent, data: { title: 'Formulas-Medicas' }},
+       { path: 'historias-medicas', component: HistoriasMedicasComponent, data: { title: 'Historias-Medicas' }},
+       { path: 'medicamentos', component: MedicamentosComponent, data: { title: 'Medicamentos' }}
     ]
   },
   { path: '**', redirectTo: 'inicio' }
