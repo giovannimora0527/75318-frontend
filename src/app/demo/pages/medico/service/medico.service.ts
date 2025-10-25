f10e74a6fcb676841e1532cce1c53514eb8f22dc
luisozun <ozunaluis2001@gmail.com>
2025-10-25 10:14:28 -0500

    Clean up medico.service.ts and fix comments
    Removed merge conflict markers and updated comments for methods.

--- src/app/demo/pages/medico/service/medico.service.ts
+++ src/app/demo/pages/medico/service/medico.service.ts
@@ -5,10 +5,6 @@ import { environment } from 'src/environments/environment';
 import { Medico } from '../models/medico';
 import { RespuestaRs } from '../../usuario/models/respuesta-rs';
 
-<<<<<<< HEAD
-=======
-
->>>>>>> 00707d3287baa9aff4df8c1f76e78b24bd7625a3
 @Injectable({
   providedIn: 'root'
 })
@@ -22,24 +18,10 @@ export class MedicoService {
     return this.backendService.get(this.urlBase, this.endpoint, 'listar');
   }
 
-<<<<<<< HEAD
-=======
-    /**
-   * Guarda un nuevo médico
-   * @param medico Datos del médico a guardar
-   */
->>>>>>> 00707d3287baa9aff4df8c1f76e78b24bd7625a3
   guardarMedico(medico: Medico): Observable<RespuestaRs> {
     return this.backendService.post(this.urlBase, this.endpoint, 'guardar', medico);
   }
 
-<<<<<<< HEAD
-=======
-  /**
-   * Actualiza un médico existente
-   * @param medico Datos del médico a actualizar
-   */
->>>>>>> 00707d3287baa9aff4df8c1f76e78b24bd7625a3
   actualizarMedico(medico: Medico): Observable<RespuestaRs> {
     return this.backendService.post(this.urlBase, this.endpoint, 'actualizar', medico);
   }
