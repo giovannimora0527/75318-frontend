3be4a40d1e5d17542a41943b95aaf1f5fa3ed904
Mateo-FG <mateoforerog3@gmail.com>
2025-10-25 22:57:24 -0500

    Update util-api.service.ts

--- src/app/services/common/util-api.service.ts
+++ src/app/services/common/util-api.service.ts
@@ -1,7 +1,11 @@
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
 import { BackendService } from '../backend.service';
-import { Especializacion } from 'src/app/demo/pages/medico/models/especializacion';
+import { Especializacion } from "src/app/demo/pages/especializacion/models/especializacion";
+import { EspecializacionService } from 'src/app/demo/pages/especializacion/service/especializacion.service';
+
+import { Cita } from 'src/app/demo/pages/cita/models/cita';
+import { Medicamento } from 'src/app/demo/pages/medicamento/models/medicamento';
 import { Observable } from 'rxjs';
 
 @Injectable({
@@ -10,9 +14,17 @@ import { Observable } from 'rxjs';
 export class UtilApiService {
   urlBase = environment.apiUrl;
 
-  constructor(private readonly backendService: BackendService) {}
+  constructor(private readonly backendService: BackendService, private readonly especializacionService: EspecializacionService) { }
 
   listarEspecializaciones(): Observable<Especializacion[]> {
-    return this.backendService.get(this.urlBase, 'especializacion', 'listar');
+    return this.especializacionService.listarEspecializaciones();
+  }
+
+  listarCitas(): Observable<Cita[]> {
+    return this.backendService.get(this.urlBase, 'cita', 'listar');
+  }
+
+  listarMedicamentos(): Observable<Medicamento[]> {
+    return this.backendService.get(this.urlBase, 'medicamento', 'listar');
   }
 }
