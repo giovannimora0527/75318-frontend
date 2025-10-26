a9c2cf141fd271bc29b21c547f0e6a9af8bc9ea6
estevez10 <juanpabloestevez98@gmail.com>
2025-10-25 20:21:31 -0500

    Implement EspecializacionService for API interactions

---
+++ src/app/demo/especializacion/service/especializacion.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { BackendService } from 'src/app/services/backend.service'; 
+import { environment } from 'src/environments/environment';
+import { Especializacion } from '../models/especializacion'; 
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EspecializacionService {
+  urlBase = environment.apiUrl; 
+  endpoint: string = 'especializacion'; 
+
+  constructor(private readonly backendService: BackendService) {}
+
+  listarEspecializaciones(): Observable<Especializacion[]> {
+    return this.backendService.get(this.urlBase, this.endpoint, 'listar');
+  }
+
+  guardarOActualizar(especializacion: Especializacion): Observable<Especializacion> {
+
+    return this.backendService.post(this.urlBase, this.endpoint, 'guardar', especializacion);
+  }
+
+  buscarPorCodigo(codigo: string): Observable<Especializacion> {
+    const urlConCodigo = `buscar-por-codigo?codigo=${codigo}`;
+    return this.backendService.get(this.urlBase, this.endpoint, urlConCodigo);
+  }
+  
+  
+
+ 
+}
