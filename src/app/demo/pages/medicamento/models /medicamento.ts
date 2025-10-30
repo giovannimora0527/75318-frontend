813c30621b085d6d1c2090d0d6ffbb34722ed522
mauricio-xf <mauricio1234madera@gmail.com>
2025-10-30 15:56:14 -0500

    Create medicamento.ts

---
+++ src/app/demo/pages/medicamento/models /medicamento.ts
@@ -0,0 +1,10 @@
+export class Medicamento {
+  id!: number;
+  nombre!: string;
+  descripcion?: string;
+  presentacion?: string;
+  fechaCompra!: string | Date;
+  fechaVence!: string | Date;
+  fechaCreacionRegistro?: string | Date;
+  fechaModificacionRegistro?: string | Date;
+}
