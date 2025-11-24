f81cd399a73ef132caa02fac6e34c16ba557264f
estevez10 <juanpabloestevez98@gmail.com>
2025-11-23 21:11:41 -0500

    Delete src/app/demo/pages/auditoria/models directory

--- src/app/demo/pages/auditoria/models/auditoria-log.ts
+++
@@ -1,9 +0,0 @@
-export interface AuditoriaLog {
-  fechaHora: string;        // ISO 8601
-  usernameIngresado: string;
-  motivo: string;           // Ej: "LOGIN_EXITOSO", "INTENTO_LOGIN_FALLIDO"
-  exitoso: boolean;
-  descripcionError?: string;
-  ipAddress: string;
-  userAgent: string;
-}
