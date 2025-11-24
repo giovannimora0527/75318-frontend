98f93231c15142b16f9cd45b6a14eb6dd882401b
estevez10 <juanpabloestevez98@gmail.com>
2025-11-23 21:49:49 -0500

    Update medicamento.ts

--- src/app/demo/pages/medicamento/models/medicamento.ts
+++ src/app/demo/pages/medicamento/models/medicamento.ts
@@ -1,8 +1,8 @@
 export class Medicamento {
-  id!: number;
-  nombre!: string;
-  descripcion?: string;
-  presentacion?: string;
+    id!: number;
+    nombre!: string;
+    descripcion!: string;
+    presentacion!: string;
   fechaCompra!: string | Date;
   fechaVence!: string | Date;
   fechaCreacionRegistro?: string | Date;
